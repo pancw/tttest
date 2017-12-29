3a60c9ce6954d8adc478219cffe7dfb0171c14a8
pancw <549191530@qq.com>
2017-12-29 23:31:10 +0800

    change

--- logic/lib/Aho-Corasick.lua
+++ logic/lib/Aho-Corasick.lua
@@ -1,7 +1,6 @@
 function chsize(char)
-    if not char then
-        return 0
-    elseif char > 240 then
+	assert(char)
+    if char > 240 then
         return 4
     elseif char > 225 then
         return 3
@@ -11,8 +10,8 @@ function chsize(char)
         return 1
     end
 end
-
 ------------------------------------------------utf8
+
 local test = {
 	"abcrr",
 	"bc",
@@ -21,7 +20,8 @@ local test = {
 	"好吗",
 	"你好",
 }
-local input = "不好你好吗bbbbdbc"
+local input = "不好你好吗bcccbbabcrrrrbdbc"
+------------------------------------------------test data
 
 function createNode()
 	return {
@@ -66,20 +66,21 @@ end
 function getAllChildrenTbl(node)
 	return node._allChildrenTbl
 end
+------------------------------------------------func
 
-local root = createNode()
+local root = createNode() -- global
 
 function add(s)
-	if (not s) or (#s == 0) then
+	if (not s) or (string.len(s) == 0) then
 		return
 	end	
 
 	local now = root
-	local i = 1
-	while (i <= #s) do
-		local utfLen = chsize(string.byte(s, i))
-		local c = s:sub(i, utfLen + i - 1)
-		i = i + utfLen
+	local idx = 1
+	while (idx <= #s) do
+		local utfLen = chsize(string.byte(s, idx))
+		local c = s:sub(idx, utfLen + idx - 1)
+		idx = idx + utfLen
 
 		if not hasChild(now, c) then
 			addChild(now, c)	
@@ -96,13 +97,15 @@ function build_tree()
 end
 
 function build_fail_pointer()
+	-- bfs
 	local queue = {root}	
 	while (#queue > 0) do
 		local now = table.remove(queue, 1) -- pop
 		local allChildrenTbl = getAllChildrenTbl(now)		
 
-		-- c儿子的失败指针=父亲的失败指针指向的节点的儿子c
+		-- 儿子c的失败指针=父亲的失败指针指向的节点的儿子c
 		-- 找不到就指向root
+		-- 第一层节点默认指向root
 		for c, node in pairs(allChildrenTbl) do
 			local fail = getFailPointer(now)
 			while ( fail and (not hasChild(fail, c)) ) do
@@ -121,11 +124,11 @@ end
 
 function try_match(input)
 	local now = root
-	local i = 1
-	while (i <= #input) do
-		local utfLen = chsize(string.byte(input, i))
-		local c = input:sub(i, utfLen + i -1)
-		i = i + utfLen
+	local idx = 1
+	while (idx <= #input) do
+		local utfLen = chsize(string.byte(input, idx))
+		local c = input:sub(idx, utfLen + idx -1)
+		idx = idx + utfLen
 
 		while ((not hasChild(now, c)) and (now ~= root)) do
 			now = getFailPointer(now)
@@ -144,7 +147,7 @@ function try_match(input)
 				if getWord(t) then
 					print("matched:", getWord(t))
 				end
-				t = getFailPointer(t)
+				t = getFailPointer(t) -- 往回找有可能匹配的子串
 			end
 		--end
 	end
